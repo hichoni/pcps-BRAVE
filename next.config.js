6b2aaac23a325d76905c4276b6e5052073b341eb
박희찬 <choni0408@gmail.com>
2025-06-24 10:59:44 +0000

    Error: Configuring Next.js via 'next.config.ts' is not supported. Please

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const withPWA = require('@ducanh2912/next-pwa');
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = pwaConfig(nextConfig);
